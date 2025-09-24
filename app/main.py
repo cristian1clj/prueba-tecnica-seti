5cd368fff7622c59b8f1a5addc7d92c8cb3226ba
cristian1clj <cristian1clj@gmail.com>
2025-09-24 15:39:52 -0500

    mejorar respuesta de errores

--- app/main.py
+++ app/main.py
@@ -13,6 +13,20 @@ app.include_router(message_controller.router)
 
 @app.exception_handler(RequestValidationError)
 async def exception_handler(request, exc):
+    error = exc.errors()[0]
+    custom_details = []
+
+    error_type = error['type']
+    field = error['loc'][1]
+    if error_type == 'missing':
+        details = f'El campo {field} es obligatorio'
+    elif error_type == 'string_type':
+        details = f'El campo {field} debe ser un string'
+    elif error_type == 'datetime':
+        details = f'El campo {field} debe ser un datetime'
+    else:
+        details = error['msg']
+    
     return JSONResponse(
         status_code=400,
         content={
@@ -20,7 +34,7 @@ async def exception_handler(request, exc):
             'error': {
                 "code": "INVALID_FORMAT",
                 "message": "Formato de mensaje inválido",
-                "details": exc.errors()[0]['msg']
+                "details": details
             }
         }
     )
